c06ca90744011d01221d3db1b4c3c3485b149b9f
Kaixiang Zhao <35736935+kzhao1228@users.noreply.github.com>
2020-06-03 08:12:14 +0100

    Update core.py

--- core.py
+++ core.py
@@ -2,16 +2,13 @@ from serial.tools.list_ports import comports
 import platform
 import usb
 import os
-from .port import Port
-
-    
+from .port import Port    
 def find_stages():
     serial_ports = [(x[0], x[1], dict(y.split('=', 1) for y in x[2].split(' ') if '=' in y)) for x in comports()]
     #print(serial_ports[0][2]['SER'])
     # expected output is [('/dev/ttyUSB0', 'APT DC Motor Controller', {'VID:PID': '0403:FAF0', 'SER': '83844171', 'LOCATION': '1-1.1'}),
     # ('/dev/ttyAMA0', 'ttyAMA0', {})]
     # each identified serial port is expressed within a tuple, i.e. ('','','').
- 
     # The loop will find all matched devices and store them in a list called dev
     dev = []
     for dev_ini in usb.core.find(find_all=True, custom_match= lambda x: x.bDeviceClass != 9):
@@ -43,7 +40,6 @@ def find_stages():
     # we make sure at each iteration only one port entry is contained in port candidates
     # i.e. the ports are accessed one by one NOT all at one go.
     assert len(port_candidates) == 1
-        
     port = port_candidates[0]
     p = Port.create(port, dev[1].serial_number)
     for stage in p.get_stages().values():
